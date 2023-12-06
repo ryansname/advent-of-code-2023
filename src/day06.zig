c7cae0e9db9126f01504561e753a76ea76dea106
Ryan Lewis <ryansname@gmail.com>
2023-12-06 19:11:55 +1300

    Cleanup Day 6 because I like the code

--- src/day06.zig
+++ src/day06.zig
@@ -6,24 +6,21 @@ const mem = std.mem;
 const std = @import("std");
 
 pub fn main() !void {
-    const part_1, _ = try parts(undefined, .{
+    const part_1 = parts(.{
         .{ .t = 61, .d = 430 },
         .{ .t = 67, .d = 1036 },
         .{ .t = 75, .d = 1307 },
         .{ .t = 71, .d = 1150 },
     });
-    const part_2, _ = try parts(undefined, .{
+    const part_2 = parts(.{
         .{ .t = 61677571, .d = 430103613071150 },
     });
     log.info("Part 1: {}", .{part_1});
     log.info("Part 2: {}", .{part_2});
 }
 
-fn parts(alloc: mem.Allocator, input: anytype) !struct { u64, u64 } {
-    _ = alloc;
-
-    var part_1: u64 = 1;
-    var part_2: u64 = 0;
+fn parts(input: anytype) u64 {
+    var result: u64 = 1;
 
     inline for (input) |race| {
         var perms: u64 = 0;
@@ -34,20 +31,19 @@ fn parts(alloc: mem.Allocator, input: anytype) !struct { u64, u64 } {
                 perms += 1;
             }
         }
-        part_1 *= perms;
+        result *= perms;
     }
 
-    return .{ part_1, part_2 };
+    return result;
 }
 
 test "simple test" {
-    var alloc = std.testing.allocator;
-    const part_1, _ = try parts(alloc, .{
+    const part_1 = parts(.{
         .{ .t = 7, .d = 9 },
         .{ .t = 15, .d = 40 },
         .{ .t = 30, .d = 200 },
     });
-    const part_2, _ = try parts(alloc, .{
+    const part_2 = parts(.{
         .{ .t = 71530, .d = 940200 },
     });
     try std.testing.expectEqual(@as(u64, 288), part_1);
