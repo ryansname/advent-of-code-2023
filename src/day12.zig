68555d37860db52749215d198c2d4ea155810cd6
Ryan Lewis <ryansname@gmail.com>
2023-12-15 16:14:59 +1300

    Day 12 all done!

--- src/day12.zig
+++ src/day12.zig
@@ -5,242 +5,114 @@ const math = std.math;
 const mem = std.mem;
 const std = @import("std");
 
-// const print = std.debug.print;
-const print = printToVoid;
-
-fn printToVoid(comptime a: anytype, b: anytype) void {
-    _ = a;
-    _ = b;
-}
-
 fn iterLines(string: []const u8) mem.TokenIterator(u8, .scalar) {
     return mem.tokenizeScalar(u8, string, '\n');
-}
-
-fn iterTokens(string: []const u8) mem.TokenIterator(u8, .scalar) {
-    return mem.tokenizeScalar(u8, string, ' ');
 }
 
 const INPUT = @embedFile("inputs/day12.txt");
 
 pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
+
     const result = try part1(INPUT);
     const result_2 = try part2(INPUT);
 
-    std.debug.assert(result_2.part2 > 41477449305);
-
-    log.info("Part 1: {}", .{result.part1});
-    log.info("Part 2: {}", .{result_2.part2});
+    try stdout.print("Part 1: {}\n", .{result.part1});
+    try stdout.print("Part 2: {}\n", .{result_2.part2});
 }
 
 const Element = enum { good, bad, unknown };
-
-const PartiallyValidResult = union(enum) {
-    invalid: void,
-    partially_valid: struct {
-        required_bad: ?usize,
-        required_chars_to_finish: usize,
-        sequences_left: usize,
-    },
-};
-fn isPartiallyValid(records: []const Element, sequences: []const u64) PartiallyValidResult {
-    var in_bad = false;
-    var expected_bads: u64 = if (sequences.len == 0) 0 else sequences[0];
-    var sequence_idx: usize = 1;
-
-    for (records) |record| {
-        switch (record) {
-            // .unknown => return .{ .required_bad = if (in_bad and expected_bads > 0) expected_bads else null },
-            .unknown => unreachable,
-            .good => {
-                if (in_bad) {
-                    if (expected_bads > 0) return .invalid;
-                    in_bad = false;
-                    if (sequence_idx < sequences.len) {
-                        expected_bads = sequences[sequence_idx];
-                        sequence_idx += 1;
-                    }
-                }
-            },
-            .bad => {
-                if (!in_bad) in_bad = true;
-                if (expected_bads == 0) return .invalid;
-                expected_bads -= 1;
-            },
-        }
-    }
-
-    var required_chars_to_finish = expected_bads;
-    if (sequences.len > 0) {
-        for (sequences[sequence_idx..]) |sequence| {
-            required_chars_to_finish += 1 + sequence;
-        }
-    }
-
-    return .{
-        .partially_valid = .{
-            .required_bad = if (in_bad and expected_bads > 0) expected_bads else null,
-            .required_chars_to_finish = required_chars_to_finish,
-            .sequences_left = if (sequences.len > 0) sequences.len - sequence_idx else 0,
-        },
-    };
-}
-
-fn areRecordsValid(records: []const Element, sequences: []const u64) bool {
-    for (records) |r| std.debug.assert(r != .unknown);
-    var fail_iter = std.mem.tokenizeScalar(Element, records, .good);
-
-    for (sequences) |sequence| {
-        if (fail_iter.next()) |fails| {
-            if (fails.len != sequence) return false;
-        } else return false;
-    }
-    return fail_iter.next() == null;
-}
-
-fn printRecords(prefix: []const u8, records: []const Element, suffix: []const u8) void {
-    print("{s} ", .{prefix});
-    printRecordsOnly(records);
-    print(" {s}\n", .{suffix});
-}
-fn printRecordsOnly(records: []const Element) void {
-    for (records) |r| print("{s}", .{switch (r) {
-        .unknown => "?",
-        .good => ".",
-        .bad => "#",
-    }});
-}
 
 fn countPossibilitiesStart(records_with_unknowns: []const Element, sequences: []const u64) u63 {
     var workspace = [_]Element{.good} ** 128;
     var result_cache: [128][128]?u63 = .{.{null} ** 128} ** 128;
 
-    printRecords("Processing", records_with_unknowns, "");
-
-    const result = countPossibilities(
+    const result = countPossibilities2(
         records_with_unknowns,
         sequences,
         &result_cache,
         workspace[0..records_with_unknowns.len],
-        0,
+        workspace[0..records_with_unknowns.len],
     );
-
-    print("For smaller sequence: ", .{});
-    printRecordsOnly(records_with_unknowns);
-    print(" got {} results\n", .{result});
 
     return result;
 }
 
-/// result_cache: [records_left][sequences_left]count
-fn countPossibilities(records_with_unknowns: []const Element, sequences: []const u64, result_cache: [][128]?u63, records: []Element, idx: usize) u63 {
-    if (idx == 0) {
-        printRecordsOnly(records_with_unknowns);
-        print("\n", .{});
+fn countPossibilities2(input: []const Element, remaining_damaged_lengths: []const u64, result_cache: [][128]?u63, workspace: []Element, solution: []Element) u63 {
+    if (result_cache[workspace.len][remaining_damaged_lengths.len]) |count| return count;
+
+    if (input.len == 0) {
+        const result: u63 = if (remaining_damaged_lengths.len == 0) 1 else 0;
+        return result;
     }
 
-    if (idx >= records.len) {
-        if (areRecordsValid(records, sequences)) {
-            print("Found valid sequence ", .{});
-            printRecordsOnly(records);
-            print("\n", .{});
-            return 1;
-        } else {
-            print("Found invalid sequence ", .{});
-            printRecordsOnly(records);
-            print("\n", .{});
-            return 0;
-        }
-    }
-
-    const offset = offset: for (records_with_unknowns[idx..], records[idx..], 0..) |record, *item, offset| {
-        switch (record) {
-            .good, .bad => item.* = record,
-            .unknown => break :offset offset,
-        }
-    } else {
-        // Reached the end, recurse and check for total validity
-        return countPossibilities(records_with_unknowns, sequences, result_cache, records, records.len);
-    };
-
-    const first_unknown_idx = idx + offset;
-    const validity = isPartiallyValid(records[0..first_unknown_idx], sequences);
-    const partial = switch (validity) {
-        .invalid => {
-            printRecordsOnly(records[0..first_unknown_idx]);
-            print(" is not partially valid for {any}\n", .{sequences});
-            return 0;
-        },
-        .partially_valid => |p| blk: {
-            if (p.required_chars_to_finish + first_unknown_idx > records.len) {
-                return 0;
-            } else break :blk p;
+    var considerations_buf: [2]Element = undefined;
+    considerations_buf[0] = input[0];
+    const considerations = switch (input[0]) {
+        .bad, .good => considerations_buf[0..1],
+        .unknown => blk: {
+            considerations_buf[0] = .good;
+            considerations_buf[1] = .bad;
+            break :blk considerations_buf[0..];
         },
     };
-    // printRecords(records[0..first_unknown_idx]);
-    // print(" is partially valid for {any}\n", .{sequences});
 
-    if (partial.required_bad) |required_bad| {
-        if (first_unknown_idx + required_bad > records_with_unknowns.len) return 0;
+    var result: u63 = 0;
+    c: for (considerations) |state| {
+        workspace[0] = state;
 
-        for (records[first_unknown_idx .. first_unknown_idx + required_bad], records_with_unknowns[first_unknown_idx .. first_unknown_idx + required_bad], 0..) |*r, src, i| switch (src) {
-            .unknown, .bad => r.* = .bad,
+        switch (workspace[0]) {
             .good => {
-                printRecordsOnly(records[0 .. first_unknown_idx + i]);
-                print(" could not add enough bad items, added {}, needed {}!\n", .{ i, required_bad });
-                return 0;
+                const count = countPossibilities2(input[1..], remaining_damaged_lengths, result_cache, workspace[1..], solution);
+                result_cache[workspace.len - 1][remaining_damaged_lengths.len] = count;
+                result += count;
             },
-        };
+            .bad => {
+                if (remaining_damaged_lengths.len == 0) continue :c;
 
-        const idx_that_must_be_good = first_unknown_idx + required_bad;
-        var next_idx = if (idx_that_must_be_good < records.len) blk: {
-            switch (records_with_unknowns[idx_that_must_be_good]) {
-                .unknown, .good => records[idx_that_must_be_good] = .good,
-                .bad => {
-                    printRecordsOnly(records[0 .. idx_that_must_be_good + 1]);
-                    print(" could not add good item spacer\n", .{});
-                    return 0;
-                },
-            }
-            break :blk idx_that_must_be_good + 1;
-        } else idx_that_must_be_good;
+                const required_bad_count = remaining_damaged_lengths[0];
+                if (input.len < required_bad_count) continue :c; // Not enough input to fulfill bad count
 
-        print("Added {} required bads ", .{required_bad});
-        printRecordsOnly(records[0..next_idx]);
-        print("\n", .{});
+                // Set the next required_bad_count elements to bad
+                for (input[0..required_bad_count], workspace[0..required_bad_count]) |input_element, *workspace_element| {
+                    switch (input_element) {
+                        .good => continue :c,
+                        .unknown, .bad => workspace_element.* = .bad,
+                    }
+                }
 
-        std.debug.assert(records[idx_that_must_be_good] == .good);
-        if (result_cache[idx_that_must_be_good][partial.sequences_left]) |count| {
-            // std.debug.print("Cache Hit!\n", .{});
-            return count;
+                // Now, if there is no more characters at all, we'll recurse and check that we're done
+                std.debug.assert(input.len >= required_bad_count);
+                if (input.len == required_bad_count) {
+                    const count = countPossibilities2(
+                        input[required_bad_count..],
+                        remaining_damaged_lengths[1..],
+                        result_cache,
+                        workspace[required_bad_count..],
+                        solution,
+                    );
+                    result += count;
+                } else {
+                    switch (input[required_bad_count]) {
+                        .unknown, .good => {
+                            workspace[required_bad_count] = .good;
+                            const count = countPossibilities2(
+                                input[required_bad_count + 1 ..],
+                                remaining_damaged_lengths[1..],
+                                result_cache,
+                                workspace[required_bad_count + 1 ..],
+                                solution,
+                            );
+                            result += count;
+                        },
+                        .bad => continue :c,
+                    }
+                }
+            },
+            .unknown => unreachable,
         }
-
-        const count = countPossibilities(records_with_unknowns, sequences, result_cache, records, next_idx);
-        result_cache[idx_that_must_be_good][partial.sequences_left] = count;
-
-        return count;
-    } else {
-        var count: u63 = 0;
-        inline for (.{ .good, .bad }) |option| {
-            records[first_unknown_idx] = option;
-            print("Trying ", .{});
-            printRecordsOnly(records[0 .. first_unknown_idx + 1]);
-            print("\n", .{});
-
-            const this_count = if (option == .good) blk: {
-                std.debug.assert(records[first_unknown_idx] == .good);
-                if (result_cache[first_unknown_idx][partial.sequences_left]) |c| break :blk c;
-
-                const c = countPossibilities(records_with_unknowns, sequences, result_cache, records, first_unknown_idx + 1);
-                result_cache[first_unknown_idx][partial.sequences_left] = c;
-                break :blk c;
-            } else countPossibilities(records_with_unknowns, sequences, result_cache, records, first_unknown_idx + 1);
-
-            count += this_count;
-        }
-
-        return count;
     }
+    return result;
 }
 
 fn part1(input: []const u8) !struct { part1: i64, part2: i64 } {
@@ -258,7 +130,6 @@ fn part1(input: []const u8) !struct { part1: i64, part2: i64 } {
         var node = progress.start(line, 0);
         node.activate();
         defer node.end();
-        std.debug.print("'{s}'\n", .{line});
 
         defer next_record_idx = 0;
         defer next_sequence_idx = 0;
@@ -385,4 +256,8 @@ test "simple test part2" {
 
 test "slow1" {
     _ = try part2("???????#?#?#??#??. 1,10,2");
+}
+
+test "slow2" {
+    _ = try part2("??.??????#???.?# 1,1,3,1,2");
 }
